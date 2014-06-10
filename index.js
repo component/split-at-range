32cb9c0590b99122c00f5bfc475de3e25a17db13
Marco Aurelio <thecoreh@gmail.com>
2014-06-10 17:52:10 -0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * Splits an HTMLElement at the point marked by the range.
+ *
+ * @param {HTMLElement} el
+ * @param {Range} range
+ * @returns {Array} array containing left and right hand sides
+ */
+
+function splitAtRange(el, range) {
+  var left = document.createRange();
+  left.selectNode(el);
+  left.setEnd(range.endContainer, range.endOffset);
+  var right = document.createRange();
+  right.selectNode(el);
+  right.setStart(range.startContainer, range.startOffset);
+  return [left.cloneContents(), right.cloneContents()];
+}
+
+/**
+ * Module Exports
+ */
+
+module.exports = splitAtRange;
